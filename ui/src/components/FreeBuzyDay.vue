ede1926c79cee072458abfec8ec1979cd5039880
benoit74 <benoit74@users.noreply.github.com>
2025-05-07 14:44:37 +0000

    Use meetDateTime to compute freebuzy status

--- ui/src/components/FreeBuzyDay.vue
+++ ui/src/components/FreeBuzyDay.vue
@@ -28,7 +28,7 @@ const getFreeBusyStatus = function (
   for (const assignement of main.getVolunteerAssignments(volunteer.id)) {
     const shift = main.shifts.filter((shift) => shift.id == assignement.shiftId)[0]
 
-    if (shift.startDateTime <= currentDate && shift.endDateTime >= currentDate) {
+    if (shift.meetDateTime <= currentDate && shift.endDateTime >= currentDate) {
       return FreeBusyStatus.Busy
     }
   }
